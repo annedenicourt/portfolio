32b7827ecb73b379c0cc554a9595129c3efd116e
Anne Denicourt <annedenicourt@gmail.com>
2021-07-16 19:01:47 +0200

    first commit

---
+++ Components/Banner.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+export default function Banner() {
+    const router = useRouter();
+
+    return (  
+        <div className="mb-5">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid justify-content-center">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-center mb-2 fs-5" id="navbarNav">
+                        <ul className="navbar-nav text-center fs-4">
+                            <li className="nav-item me-3">
+                                <Link href="/"><a className= {router.pathname == "/" ? `nav-link ${styles.active}` : "nav-link"}>Accueil</a></Link>
+                            </li>
+                            <li className="nav-item me-3">
+                                <Link href="/qui-suis-je"><a className= {router.pathname == "/qui-suis-je" ? `nav-link ${styles.active}` : "nav-link"}>Qui suis-je ?</a></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="/mon-cv"><a className= {router.pathname == "/mon-cv" ? `nav-link ${styles.active}` : "nav-link"}>Mon CV</a></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="/contact"><a className= {router.pathname == "/contact" ? `nav-link ${styles.active}` : "nav-link"}>Contact</a></Link>
+                            </li>                       
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import Head from 'next/head'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return ( <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+      />
+      <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+      crossorigin="anonymous"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@300&family=Poiret+One&family=Zen+Loop&display=swap" rel="stylesheet"></link>
+      <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
+      integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi"
+      crossorigin="anonymous"></script>
+      <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
+      integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG"
+      crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    </Head>
+
+    <Component {...pageProps} /> 
+</>
+  )
 }
 
 export default MyApp
