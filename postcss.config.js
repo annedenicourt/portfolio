05424c90e78a74e55f2bfe824fb8f84074c2cd39
Anne Denicourt <annedenicourt@gmail.com>
2024-02-10 10:07:41 +0100

    fix tailwind ter

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-/* module.exports = {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} */
-
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    //require("@tailwindcss/deprecation-warnings"),
-    require("autoprefixer"),
-  ],
 };
